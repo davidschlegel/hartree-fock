3a06b129e0e755d9a605830e2331b87bc5ca196f
David Schlegel <david.schlegel@stud.uni-goettingen.de>
2015-10-29 23:29:01 +0100

    Simplified 2d method

--- 2d_finite_diff.hs
+++ 2d_finite_diff.hs
@@ -21,29 +21,27 @@ import DSP.Basic
 --gridsize
 n = 60
 
---first blockmatrix
-block1 = buildMatrix n n (\(i,j) -> if (i,j) == (i,i) then 4 else if (i,j) == (i, i-1) then -1 else if (i,j) == (i,i+1) then -1 else 0) 
-block1_Sparse = mkSparse [((i,j), (block1 @@> (i,j))) | (i,j) <- (find (/= 0) block1)]
+--index function
+index :: (Int, Int) -> Int -> Double
+index (i,j) n
+	| i == j       =  4
+	| i == j + n   = -1
+	| i == j - n   = -1
+	| i == j + 1   = -1
+	| i == j - 1   = -1
+	| otherwise    =  0
 
---second blockmatrix
-block2 = -1 * ident n
-block2_Sparse = mkSparse [((i,j), (block2 @@> (i,j))) | (i,j) <- (find (/= 0) block2)]
+-- Laplace Matrix in 2 dimensions
+t :: Matrix Double
+t = buildMatrix (n*n) (n*n) (\(i,j) -> index (i,j) n)
+	
 
---function mapping three matrices to a tridiagonal matrix of desired dimension
-tridiag ::  Int -> Matrix Double -> Matrix Double -> Matrix Double -> Matrix Double
-tridiag n l m r = a + b + c
-	where 
-		sizeb = fst (size l)
-		sizec = fst (size r)
-		a = diagBlock (replicate n m)
-		b = (zeros sizeb (sizeb^2)) === ((diagBlock (replicate (n-1) l)) |||  (zeros (sizeb^2 - sizeb) sizeb ))
-		c = ((zeros (sizec^2 -n) sizec ) ||| (diagBlock (replicate (n-1) r)))  === (zeros sizec (sizec^2))
-		
-		
-t = tridiag n block2 block1 block2		
+-- Compute eigenvalues and corresponding eigenvectors 
 e = sort(toList(mapVector realPart (fst(eig(t)))))
 psi = toColumns (mapMatrix realPart (snd(eig(t))))
 
+
+--For further image processing
 grid :: Vector Double
 grid = fromList (linspace 0.0 1.0 n)
 
