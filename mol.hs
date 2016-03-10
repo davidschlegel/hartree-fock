fa640cf883204a5e75395553b9452dede8c691af
David Schlegel <david.schlegel@stud.uni-goettingen.de>
2016-03-10 22:30:47 +0100

    Added mol.hs: Molecule data type and info function

---
+++ mol.hs
@@ -0,0 +1,44 @@
+module Mol where
+
+import Read_basis
+
+--data type Orbital: Contains all necessary information about a certain orbital
+data Mol = Mol {  molname :: String		--molecule name , eg "H20" etc.
+		, config :: [(Atom , [Double])] --configuration
+		} deriving (Show)
+
+geometry :: Mol -> [[Double]]
+geometry mol = geom k
+	where 	k = length $ config mol
+		geom n 
+			| n == 0 = []
+			| otherwise  = snd ((config mol) !! (k-n)) : geom (n-1)
+
+
+
+--Show all Important Information about the given Molecule
+--Input: Mol
+--Output: 
+--	Molecule Name
+--	Number of Atoms
+--	For each Atom:
+--		Atomname
+--		Number of Orbitals
+--		contracted set descriptions
+--		geometry
+molinfo :: Mol -> IO ()
+molinfo mol = do
+	putStrLn ""
+	putStrLn ("Molecule name:   " ++ molname mol)
+	putStrLn ("Number of Atoms:  " ++ show k)
+	printAtoms mol k
+		where 	k = length $ config mol
+			printAtoms mol 0 = putStrLn " "
+		 	printAtoms mol n = do 
+				putStrLn ("\tAtom:    " ++ atomname ( fst ( (config mol) !! (n-1))))
+				putStrLn  ("\t \t Number of Orbitals:" ++  "\t" ++ show (length ( orbitals ( fst ( (config mol) !!(n-1)))) ))
+				putStrLn ("\t \t contracted sets:" ++ "\t" ++ show( [description k | k <- orbitals (fst ( (config mol) !! (n-1)))]))
+				putStrLn ("\t \t geometry:" ++ "\t \t" ++ show( snd ( (config mol) !!(n-1)) ) )
+				--putStrLn (str ++ orb)
+				printAtoms mol (n-1)
+				
