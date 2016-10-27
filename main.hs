4afa60a8f497ba09d368bc801b38da39d2ab4de9
David Schlegel <david.schlegel@stud.uni-goettingen.de>
2016-10-27 18:32:00 +0200

    Added main file for better package handling

---
+++ main.hs
@@ -0,0 +1,17 @@
+module Main where
+{-
+--Total Packages included
+import System.Process
+import System.IO
+import System.IO.Unsafe
+import Data.List
+import Data.List.Split
+import Numeric.Container hiding (linspace)
+import Data.Maybe
+import Numeric.LinearAlgebra
+import Numeric.LinearAlgebra.Data hiding (linspace)
+-}
+import Read_basis
+import Mol
+import Gauss
+import Matrices
