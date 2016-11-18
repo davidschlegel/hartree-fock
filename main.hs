b1d54325e35542b05c4ec0c32db41ee7b500d4a6
David Schlegel <david.schlegel@stud.uni-goettingen.de>
2016-11-18 01:23:44 +0100

    changed module imports

--- main.hs
+++ main.hs
@@ -10,8 +10,18 @@ import Numeric.Container hiding (linspace)
 import Data.Maybe
 import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.Data hiding (linspace)
+import Text.Printf
 -}
-import Read_basis
-import Mol
+
+--Some Dictionaries
+--dict1 = [("CARBON", "C"), ("NITROGEN", "N"), ("OXYGEN", "O"), ("HYDROGEN", "H")]
+--dict2 = [("CARBON", 6.0), ("NITROGEN", 7.0), ("OXYGEN", 8.0), ("HYDROGEN", 1.0)]
+--dict3 = [("S", 0), ("P", 1), ("D", 2), ("F", 3)]	 
+import Data
+import ReadWrite
 import Gauss
 import Matrices
+import Numeric.Container hiding (linspace)
+
+
+
